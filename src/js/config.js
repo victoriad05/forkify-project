bda43a10d9223758888c1ab855bfe75328f827f8
victoriad05 <dunaeva.victoria@gmail.com>
2022-10-25 16:28:58 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE =10;
+export const KEY = 'f7b7c142-cf4e-4643-80cc-c1d4b57d2acb';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,18 @@
+import View from './View';
+import icons from 'url:../../img/icons.svg';
+import previewView from './previewView';
+
+class ResultsView extends View{
+    _parentElement = document.querySelector('.results');
+     _errorMessage = 'No recipes for your query! Please try again.';
+     _message = '';
+
+     _generateMarkup(){
+      return this._data
+      .map(result => previewView.render(result, false))
+      .join('');
+  }
+    
+}
+
+export default new ResultsView();
